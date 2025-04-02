85a86968fec7dbe164b1e8c1ef39112383c3d4d0
Kazu-K0032 <kuji32global@gmail.com>
2025-04-03 00:59:54 +0900

    deploy

--- api/main.go
+++ api/main.go
@@ -1,15 +1,11 @@
-package main
+package handler
 
 import (
 	"fmt"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello from Go on Vercel!")
-}
-
-func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+// 関数名は "Handler" 固定
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello from Go + Vercel Serverless!")
 }
