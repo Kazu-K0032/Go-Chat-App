57179e7942441f886ce0df195f1122f2b1bdfabc
Kazu-K0032 <kuji32global@gmail.com>
2025-04-04 02:48:02 +0900

    wip

--- app/js/settings.js
+++ app/js/settings.js
@@ -1,7 +1,14 @@
 function togglePasswordForm() {
   const form = document.querySelector(".l-settings__passwordForm");
   if (form) {
-    form.style.display = form.style.display === "none" ? "block" : "none";
+    // フォームの表示状態を切り替え
+    form.classList.toggle("is-active");
+
+    // フォームが非表示になった場合、入力をクリア
+    if (!form.classList.contains("is-active")) {
+      const inputs = form.querySelectorAll("input[type='password']");
+      inputs.forEach((input) => (input.value = ""));
+    }
   } else {
     // フォームが存在しない場合は、サーバーにリクエストを送信
     window.location.href = "/settings?show_password_form=true";
