61968dd3f9889830b450a25a5a0fc8cfdf96eaa4
Kazu-K0032 <kuji32global@gmail.com>
2025-04-13 12:52:20 +0900

    fix: conflict

---
+++ config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"log"
+	utils "security_chat_app/internal/utils/log"
+
+	"gopkg.in/go-ini/ini.v1"
+)
+
+type ConfigList struct {
+	Port     string
+	LogFile  string
+	Static   string
+	Firebase string
+}
+
+var Config ConfigList
+
+func init() {
+	LoadConfig()
+	utils.LoggingSettings(Config.LogFile)
+}
+
+func LoadConfig() {
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	Config = ConfigList{
+		Port:     cfg.Section("web").Key("port").MustString("8080"),
+		LogFile:  cfg.Section("web").Key("logfile").String(),
+		Static:   cfg.Section("web").Key("static").String(),
+		Firebase: cfg.Section("firebase").Key("serviceAccountKey").String(),
+	}
+}
