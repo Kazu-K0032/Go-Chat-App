e8fdae4f673bfb88a78df032bb37da6e1fee3d8c
Kazu-K0032 <kuji32global@gmail.com>
2025-04-20 00:04:56 +0900

    wip: コード整理

--- internal/interface/handler/logout_hander.go
+++ internal/interface/handler/logout_hander.go
@@ -11,10 +11,8 @@ import (
 // ログアウト処理を実行
 func LogoutHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-		// セッションからユーザー情報を取得
 		session, err := middleware.ValidateSession(w, r)
 		if err == nil && session != nil && session.User != nil {
-			// ユーザーの状態をオフラインに更新
 			if err := firebase.UpdateField("users", session.User.ID, "IsOnline", false); err != nil {
 				log.Printf("ユーザー状態の更新に失敗: %v", err)
 			}
